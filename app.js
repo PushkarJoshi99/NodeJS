cdda6a43ba87860102e0a37d2b2f76a446e36f6e
PushkarJoshi99 <jpushkar.2001@gmail.com>
2024-08-10 23:06:36 +0530

    31. Sending Responses

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const http = require('http');
+const http = require("http");
 
-const server = http.createServer((req,res) => {
-    console.log(req.url)
-    console.log(req.headers)
-    console.log(req.method)
-    
+const server = http.createServer((req, res) => {
+	console.log(req.url);
+	console.log(req.headers);
+	console.log(req.method);
+	res.setHeader("Content-Type", "text/html");
+	res.write("<html>");
+	res.write("<head><title>1st Node server</title></head>");
+	res.write("<body>My name is Pushkar. This is my first node server.</body>");
+	res.write("</html>");
+	res.end();
 });
 
 server.listen(3000);
