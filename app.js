9c0b10209a01ffaabb2437a038e174bc9821acb3
PushkarJoshi99 <jpushkar.2001@gmail.com>
2024-08-11 01:27:59 +0530

    33. Routing Requests

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-	console.log(req.url);
-	console.log(req.headers);
-	console.log(req.method);
+	const url = req.url;
+	if (url === "/") {
+		res.setHeader("Content-Type", "text/html");
+		res.write("<html>");
+		res.write("<head><title>Redirecting</title></head>");
+		res.write("<body><form method = 'POST' action = '/message'><button type='submit'>Click here</button></form></body>");
+		res.write("</html>");
+		return res.end();
+
+	}
 	res.setHeader("Content-Type", "text/html");
 	res.write("<html>");
 	res.write("<head><title>1st Node server</title></head>");
