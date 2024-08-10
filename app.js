a749e3029b100b305e7b286eb4330431571d3771
PushkarJoshi99 <jpushkar.2001@gmail.com>
2024-08-10 21:59:01 +0530

    30. Understanding Requests

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-    console.log(req)
-    process.exit()
+    console.log(req.url)
+    console.log(req.headers)
+    console.log(req.method)
+    
 });
 
 server.listen(3000);
