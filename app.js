b7698251ac195321df6c2f875ecb544cfcc58166
PushkarJoshi99 <jpushkar.2001@gmail.com>
2024-08-14 00:16:15 +0530

    37. Blocking and Non-Blocking Code

--- app.js
+++ app.js
@@ -19,15 +19,17 @@ const server = http.createServer((req, res) => {
             console.log(chunk)
             body.push(chunk)
         })
-        req.on('end', ()=>{
+        return req.on('end', ()=>{
             const data = Buffer.concat(body).toString()
             console.log(data)
             message = data.split('=')[1]
-            fs.writeFileSync('message.txt',message);
+            fs.writeFile('message.txt',message, (err)=>{
+                res.statusCode = 302;
+                res.setHeader('Location','/');
+                return res.end();
+            });
+
         })
-        res.statusCode = 302;
-        res.setHeader('Location','/');
-        return res.end();
     }
 
 	res.setHeader("Content-Type", "text/html");
